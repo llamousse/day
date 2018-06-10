e39dde719a4cc42ac6a1b38ac709740aea404142
Vicky <vyue013@gmail.com>
2018-06-10 02:01:18 -0700

    added travis

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+// const express = require('express');
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
+//
+// const jsonParser = bodyParser.json();
+// const app = express();
+//
+// const { testt } = require('./models');
+//
+// app.use(morgan('common'));
+// app.use(express.static('public'));
+//
+// // testt.create("Hello World");
+//
+// app.get('/', (req, res) => {
+//
+// });
+//
+// app.listen(process.env.PORT || 8080);
--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-// const faker = require('faker')
-// const mongoose = require('mongoose')
-
-// const {app, runServer, closeServer} = require('../server')
-
-chai.use(chaiHttp);
-
-const should = chai.should;
-const { app } = require('../server');
-
-// describe('load localhost:8080', function(){
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// // const faker = require('faker')
+// // const mongoose = require('mongoose')
 //
-// 	it('should go to home page', function(){
+// // const {app, runServer, closeServer} = require('../server')
+//
+// chai.use(chaiHttp);
+//
+// const should = chai.should;
+// const { app } = require('../server');
+//
+// describe('load localhost:8080', function() {
+//
+// 	it('should go to home page', function() {
 // 		return chai.request(app)
-// 			.then(function(res){
+//       .get('/')
+// 			.then(function(res) {
 // 				res.should.have.status(200);
 // 				res.should.be.html;
 // 			});
