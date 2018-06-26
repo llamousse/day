e13e82329f041fe239840791de26297698ccc0e2
Sebastian Bastidas <sbastidasr@gmail.com>
2018-06-26 09:47:16 -0500

    sidebar + form

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 $(window).on("scroll", function(){
-  console.log($(window).scrollTop());
   if($(window).scrollTop() > 350){
     $(".home-nav").css("background-color", "#94806A");
   }
@@ -9,81 +8,24 @@ $(window).on("scroll", function(){
 });
 
 // CLICK ON LOG IN
-$('.search-form').on('click', '.submit-login', function(event) {
+$('.rectangle').on('click', '.login', function(event) {
   $('#login').css("background-color", "#B0E2F8");
-  $('.submit-signup').css("background-color", "");
-  $('#firstname').addClass("hide-this");
-  $('#lastname').addClass("hide-this");
-  $('#email').prop('required', true);
-  $('#password').prop('required', true);
+  $('#signup').css("background-color", "#ffffff");
+  $('#signup-form').hide();
+  $('#login-form').show();
 });
 
 // CLICK ON SIGN UP
-$('.search-form').on('click', '.submit-signup', function(event) {
+$('.rectangle').on('click', '.signup', function(event) {
   $('#signup').css("background-color", "#B0E2F8");
-  $('#login').css("background-color", "");
-  $('#email').prop('required', true);
-  $('#password').prop('required', true);
-  $('#firstname').removeClass("hide-this").prop('required', true);
-  $('#lastname').removeClass("hide-this").prop('required', true);
+  $('#login').css("background-color", "#ffffff");
+  $('#signup-form').show();
+  $('#login-form').hide();
 });
-
-// function handleClickOn() {
-//   $('.sidebar-bg').removeClass("hide-this");
-//   $('.home-nav').css("background-color", "#94806A");
-//   $('.home-nav').css("width", "100%");
-//   $('.about-section').addClass("hide-this");
-//   $('.rectangle').addClass("hide-this");
-//   $('.display-start').addClass("hide-this");
-// }
-//
-// function handleClickOff() {
-//     $('.sidebar-bg').addClass("hide-this");
-//     $('.home-nav')
-//       .css("background-color", "#94806A");
-//       .css("width", "100%");
-//     $('.about-section').addClass("hide-this");
-//     $('.rectangle').addClass("hide-this");
-//     $('.display-start').addClass("hide-this");
-// }
-// $('.sidebar-bg').on('click', handleClickOn);
-// $('.sidebar-bg').off('click', handleClickOff);
 
 
 $('.home-nav').on('click', '.sidebar-menu', function(event) {
-  $('.sidebar-bg').removeClass("hide-this");
-  $('.home-nav').css("background-color", "#94806A");
-  // $('.home-nav').css("width", "50%");
-  // $('.about-section').addClass("hide-this");
-  // $('.rectangle').addClass("hide-this");
-  // $('.display-start').addClass("hide-this");
+  event.preventDefault()
+  $('.sidebar-bg').toggleClass("hidden");
+  $('.sidebar-top').toggleClass("hidden");
 });
-
-  // $('html, body').bind('DOMMouseScroll mousewheel MozMousePixelScroll', function(e) {
-  //   var scrollTo = 0;
-  //
-  //   if (e.type == 'mousewheel') {
-  //       scrollTo = (e.originalEvent.wheelDelta * -1);
-  //   }
-  //   else if (e.type == 'DOMMouseScroll') {
-  //       scrollTo = e.originalEvent.detail;
-  //   }
-  //
-  //   if (scrollTo > 0) {
-  //     e.preventDefault();
-  //     return false;
-  //   }
-  // });
-  //
-  // $(this).unbind('click');
-// });
-
-// $('.home-nav').off('click', '.sidebar-menu', function(event) {
-//   $('.sidebar-bg').addClass("hide-this");
-//   $('.home-nav')
-//     .css("background-color", "#94806A");
-//     .css("width", "100%");
-//   $('.about-section').addClass("hide-this");
-//   $('.rectangle').addClass("hide-this");
-//   $('.display-start').addClass("hide-this");
-// });
