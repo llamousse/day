36e88cd7bd98c298de98984634e74dfb892b60ad
Vicky <vyue013@gmail.com>
2018-06-25 18:08:42 -0700

    fix sign up/log in load page

--- public/index.js
+++ public/index.js
@@ -9,69 +9,22 @@ $(window).on("scroll", function(){
 });
 
 
+// CLICK ON LOG IN
+$('.search-form').on('click', '.submit-login', function(event) {
+  $('#login').css("background-color", "#B0E2F8");
+  $('.submit-signup').css("background-color", "");
+  $('#firstname').addClass("hide-this");
+  $('#lastname').addClass("hide-this");
+  $('#email').prop('required', true);
+  $('#password').prop('required', true);
+});
+
 // CLICK ON SIGN UP
-// $('.search-form').on('click', function() {
-
-  // if($('.submit-button').on('click')) {
-  //   $('email').attr("required", "true");
-  //   $('password').attr("required", "true");
-  // }
-
-  // if($('.submit-login').on('click')) {
-  //   $('.submit-login').css("background-color", "#B0E2F8");
-  //   $('.submit-signup').css("background-color", "");
-  //   $('#firstname').addClass("hide-this");
-  //   $('#lastname').addClass("hide-this");
-  //   $('email').attr("required", "true");
-  //   $('password').attr("required", "true");
-  // }
-
-function loadLogin() {
-  if($('.search-form').on('click', '.submit-login')) {
-    $('.submit-login').css("background-color", "#B0E2F8");
-    $('.submit-signup').css("background-color", "");
-    $('#email').prop('required', true);
-    $('#password').prop('required', true);
-  }
-
-
-
-  // $('.search-form').on('click', '.submit-login', function(event) {
-  //   $('.submit-login').css("background-color", "#B0E2F8");
-  //   $('.submit-signup').css("background-color", "");
-  //   $('email').prop('required', true);
-  //   $('password').prop('required', true);
-
-  // $('.submit-login').on('click')) {
-  //   $('.submit-login').css("background-color", "#B0E2F8");
-  //   $('.submit-signup').css("background-color", "");
-  //   // $('#firstname').addClass("hide-this");
-  //   // $('#lastname').addClass("hide-this");
-  //   $('email').attr("required", "true");
-  //   $('password').attr("required", "true");
-
-
-
-}
-  // else if ($('.submit-signup').on('click')) {
-  //   $('.submit-signup').css("background-color", "#B0E2F8");
-  //   $('.submit-login').css("background-color", "");
-  //   $('#firstname').removeClass("hide-this").attr("required", "true");
-  //   $('#lastname').removeClass("hide-this").attr("required", "true");
-  //   $('email').attr("required", "true");
-  //   $('password').attr("required", "true");
-  // }
-
-
-// });
-
-// $('.search-form').on('click', function() {
-//   if ($('.submit-signup').on('click')) {
-//     $('.submit-signup').css("background-color", "#B0E2F8");
-//     $('.submit-login').css("background-color", "");
-//     $('#firstname').removeClass("hide-this").attr("required", "true");
-//     $('#lastname').removeClass("hide-this").attr("required", "true");
-//     $('email').attr("required", "true");
-//     $('password').attr("required", "true");
-//   }
-// });
+$('.search-form').on('click', '.submit-signup', function(event) {
+  $('#signup').css("background-color", "#B0E2F8");
+  $('#login').css("background-color", "");
+  $('#email').prop('required', true);
+  $('#password').prop('required', true);
+  $('#firstname').removeClass("hide-this").prop('required', true);
+  $('#lastname').removeClass("hide-this").prop('required', true);
+});
