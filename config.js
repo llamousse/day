3d394245da2869ecf9cb2b14f03e7e0196884db1
Vicky <vyue013@gmail.com>
2018-07-23 09:46:21 -0700

    fix api key travis

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
+
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/day';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/day-test';
 exports.PORT = process.env.PORT || 8080;
